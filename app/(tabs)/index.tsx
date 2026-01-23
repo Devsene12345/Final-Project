a6f4213f1fad3195dfc914d632089488b359d7f9
Devsene12345 <senevirathnadevmin84@gmail.com>
2026-01-23 11:06:01 +0530

    commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { TreePine } from "lucide-react-native";
+import { useRouter } from "expo-router";
 
 export default function TreeMappingHome() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <TreePine size={48} color="green" />
@@ -9,6 +11,21 @@ export default function TreeMappingHome() {
       <Text style={styles.subtitle}>
         Analyze, manage & visualize urban trees
       </Text>
+
+      {/*Buttons */}
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => router.push("./login")}
+      >
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.registerBtn}
+        onPress={() => router.push("./register")}
+      >
+        <Text style={styles.btnText}>Register</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -29,5 +46,25 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#666",
     marginTop: 6,
+  },
+  loginBtn: {
+    backgroundColor: "#4CAF50",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    marginTop: 24,
+  },
+  registerBtn: {
+    backgroundColor: "#2196F3",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    marginTop: 12,
+  },
+  btnText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
